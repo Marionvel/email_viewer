5af074c4d424d5295125e530b0d6c6497f74c0ad
Roger <marion.velard@gmail.com>
2018-08-24 01:29:12 +0200

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,47 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Email.all.destroy_all
 
-Email.create(object: 'Les pommes', 
-						 body: 'This is the 1st seed email... I can say alotttt of bs but I\'ll just stop right here :)') 
-
-Email.create(object: 'Maison',
-						 body: 'Hello chou, je devrais etre de retour a la maison le 15 au soir :) je te dis ç maintenant histoire que tu ai le temps de tout ranger ;) bisous')
-
-Email.create(object: 'Super gateau',
-						 body: 'Hey regarde sur marmiton j\'ai trouvé une méga recette de gateau au crevettes tu vas kiffer *q* ...')
-
-Email.create(object: 'Nouveau silencieux sur GTA V',
-						 body: 'Nouveau sur GTA Online : Le Silencieux JVT NiKR !!!! Précommandez le pour seulement 50$ !! *offre limitée dans la limite des stocks disponibles...*')
-
-Email.create(object: '30 recettes avec des pâtes!',
-						 body: 'T\'as pas assez de thune pour manger autre choses que des pâtes ? TKT nous on a LA solution et on te propose une liste non exhaustive de 30 super recettes aves des pâtes \\o/ le lien ci dessous [redacted] ')
-
-Email.create(object: 'Nouveau : Les Sims Au Cimetière !',
-						 body: 'ENFIN le nouveau DLC des Sims est sorti ! Emmène tes sims au cimetiere fais les sauter sur des tombes pêcher dans le lac des âmes ou se faire couper la tête par la Grande Faucheuse !!! Et en ce moment, prix mini ! Achète-le pour seulement 80$ avec le code [redacted] !!') 
-
-Email.create(object: 'Pick-up',
-						 body: 'This is the 2nd seed email... huhuhu hahaha trololo')
-
-Email.create(object: 'Air-France Promo',
-						 body: 'Rendez-vous oú vous voulez gratuitement .... mais pas avec nous ;)')
-
-Email.create(object: 'Soutenez les blessés en syrie',
-						 body: 'This is the 4th seed email... 5/4 be with you  ....')
-
-Email.create(object: 'Adopte un chaton :3',
-						 body: 'MIAOU !! miaoumiamioumaoumioumaoumiaou !! .... miamiaou')
-
-Email.create(object: 'Fais pas cette tête',
-						 body: 'C\'est bon fais pas la gueule c\'était juste pour rire :/ je pensais pas que c\'était indélébile ') 
-
-Email.create(object: 'PUBG obnubile ton mec ? ...',
-						 body: 'Bonne nouvelle le mien aussi il lâche pas u.u je comprends XD')
-
-Email.create(object: 'Artemis Spectrum ! Le casque logitech qui te met bien...',
-						 body: 'Cher mais styley *q*    https://www.logitechg.fr/fr-fr/product/g633-7-1-surround-sound-gaming-headset        #nonjefaispasdepub :p ')
-
-Email.create(object: 'Techniques pour repasser une chemise',
-						 body: 'May 4th .... B w/ u XD')
-
-Email.create(object: 'J\'ai tes tongs dans mon sac', 
-						body: 'Heuuu c\'est normal qu\'il y a tes tongs dans mon sac ?')
+Email.create(object: 'Mail 1', body: 'Voici votre mail numéro 1 pour vous dire Bonjour !')
+Email.create(object: 'Mail 2', body: 'Voici votre mail numéro 2 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 3', body: 'Voici votre mail numéro 3 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 4', body: 'Voici votre mail numéro 4 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 5', body: 'Voici votre mail numéro 5 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 6', body: 'Voici votre mail numéro 6 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 7', body: 'Voici votre mail numéro 7 pour vous dire Bonjour !') 
+Email.create(object: 'Mail 8', body: 'Voici votre mail numéro 8 pour vous dire Bonjour !') 
